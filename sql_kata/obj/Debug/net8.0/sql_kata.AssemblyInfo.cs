771f34bc1a1962032550984d4fed0005e2ba6fc4
BH220 <61535918+BH220@users.noreply.github.com>
2025-12-16 14:54:18 +0900

    .

--- sql_kata/obj/Debug/net8.0/sql_kata.AssemblyInfo.cs
+++ sql_kata/obj/Debug/net8.0/sql_kata.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sql_kata")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ade69029149f525f57f9f083ff65d649598ccd0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("sql_kata")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sql_kata")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
